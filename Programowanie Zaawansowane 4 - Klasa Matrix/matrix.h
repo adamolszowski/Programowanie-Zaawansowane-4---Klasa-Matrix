77367fc5b9e50dd3ae7dc3d9c5c2e46edc223769
ADAM <ADAM@DESKTOP-9HOJFIG>
2025-12-09 14:41:41 +0100

    Określenie funkcjonalności klasy matrix

--- Programowanie Zaawansowane 4 - Klasa Matrix/matrix.h
+++ Programowanie Zaawansowane 4 - Klasa Matrix/matrix.h
@@ -1,5 +1,60 @@
 #pragma once
-class matrix
-{
+
+#include <memory>
+#include <iostream>
+
+class matrix {
+private:
+    int n;
+    std::unique_ptr<int[]> data;
+
+public:
+    matrix(void);
+    matrix(int n);
+    matrix(int n, int* t);
+    matrix(const matrix& m);
+    ~matrix(void);
+
+    matrix& alokuj(int n);
+    matrix& wstaw(int x, int y, int wartosc);
+    int pokaz(int x, int y);
+    matrix& odwroc(void);
+    matrix& losuj(void);
+    matrix& losuj(int x);
+    matrix& diagonalna(int* t);
+    matrix& diagonalna_k(int k, int* t);
+    matrix& kolumna(int x, int* t);
+    matrix& wiersz(int x, int* t);
+    matrix& przekatna(void);
+    matrix& pod_przekatna(void);
+    matrix& nad_przekatna(void);
+    matrix& szachownica(void);
+
+    matrix& operator+(matrix& m);
+    matrix& operator*(matrix& m);
+
+    matrix operator+(int a) const;
+    matrix operator*(int a) const;
+    matrix operator-(int a) const;
+
+    friend matrix operator+(int a, const matrix& m);
+    friend matrix operator*(int a, const matrix& m);
+    friend matrix operator-(int a, const matrix& m);
+
+    matrix& operator++(int);
+    matrix& operator--(int);
+
+    matrix& operator+=(int a);
+    matrix& operator-=(int a);
+    matrix& operator*=(int a);
+
+    matrix& operator()(double value);
+
+    friend std::ostream& operator<<(std::ostream& o, matrix& m);
+
+    bool operator==(const matrix& m);
+    bool operator>(const matrix& m);
+    bool operator<(const matrix& m);
 };
+
 
