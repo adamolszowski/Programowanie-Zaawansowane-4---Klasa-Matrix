2185cffeedcceb030b7ffb951808b0c8773f58e4
DawidGH <tokarczykdawid075@gmail.com>
2025-12-09 14:55:20 +0100

    Definicje podstawowych metod klasy matrix

--- Programowanie Zaawansowane 4 - Klasa Matrix/matrix.cpp
+++ Programowanie Zaawansowane 4 - Klasa Matrix/matrix.cpp
@@ -29,3 +29,69 @@ matrix::matrix(const matrix& m) : n(m.n), data(make_unique<int[]>(m.n* m.n)) {
 }
 
 matrix::~matrix(void) = default;
+
+matrix& matrix::alokuj(int n) {
+    if (!data) {
+        data = make_unique<int[]>(n * n);
+    }
+    else if (n > this->n) {
+        data = make_unique<int[]>(n * n);
+    }
+    this->n = n;
+    return *this;
+}
+
+matrix& matrix::wstaw(int x, int y, int wartosc) {
+    if (x >= 0 && x < n && y >= 0 && y < n)
+    {
+        data[x * n + y] = wartosc;
+    }
+    else {
+        cout << "Blad - nie ma takiej pozycji w macierzy";
+    }
+
+    return *this;
+}
+
+int matrix::pokaz(int x, int y) {
+    if (x >= 0 && x < n && y >= 0 && y < n)
+    {
+        return data[x * n + y];
+    }
+    else {
+        cout << "Blad - nie ma takiej pozycji w macierzy";
+        return 0;
+    }
+}
+
+matrix& matrix::odwroc(void) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++) {
+            swap(data[i * n + j], data[j * n + i]);
+        }
+    }
+    return *this;
+}
+
+matrix& matrix::losuj(void) {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    for (int i = 0; i < n * n; i++)
+    {
+        data[i] = rand() % 10;
+    }
+    return *this;
+}
+
+matrix& matrix::losuj(int x) {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    for (int i = 0; i < x; i++)
+    {
+        int row = rand() % n;
+        int col = rand() % n;
+        data[row * n + col] = rand() % 10;
+    }
+    return *this;
+}
