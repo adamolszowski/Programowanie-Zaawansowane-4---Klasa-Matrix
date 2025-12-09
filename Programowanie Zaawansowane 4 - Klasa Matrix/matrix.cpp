0d22ac64ef7d018bca5632443b7d667c893d609c
DawidGH <tokarczykdawid075@gmail.com>
2025-12-09 14:51:33 +0100

    Konstruktory & Destruktory Klasy Matrix

--- Programowanie Zaawansowane 4 - Klasa Matrix/matrix.cpp
+++ Programowanie Zaawansowane 4 - Klasa Matrix/matrix.cpp
@@ -4,3 +4,28 @@
 #include <cmath>
 
 using namespace std;
+
+matrix::matrix(void) : n(0), data(nullptr) {}
+
+matrix::matrix(int n) : n(n), data(make_unique<int[]>(n* n)) {
+    for (int i = 0; i < n * n; i++)
+    {
+        data[i] = 0;
+    }
+}
+
+matrix::matrix(int n, int* t) : n(n), data(make_unique<int[]>(n* n)) {
+    for (int i = 0; i < n * n; i++)
+    {
+        data[i] = t[i];
+    }
+}
+
+matrix::matrix(const matrix& m) : n(m.n), data(make_unique<int[]>(m.n* m.n)) {
+    for (int i = 0; i < n * n; i++)
+    {
+        data[i] = m.data[i];
+    }
+}
+
+matrix::~matrix(void) = default;
